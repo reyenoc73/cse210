33787a2686d1c598b00dcede1a0b3e3aab499694
reyenoc <ereyesramirez@byupathway.edu>
2025-06-04 20:16:08 -0600

    finished youtube

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("Exploring Oaxaca Street Food", "Chef Maria", 540);
+        video1.AddComment(new Comment("Ana", "Amazing video! I miss tlayudas."));
+        video1.AddComment(new Comment("Luis", "Great quality and delicious dishes!"));
+        video1.AddComment(new Comment("Sofia", "I love how you explain each recipe."));
+
+        Video video2 = new Video("Traditional Oaxacan Dishes", "CookingOaxaca", 680);
+        video2.AddComment(new Comment("Carlos", "Can’t wait to visit and try this!"));
+        video2.AddComment(new Comment("Laura", "Mole negro is my favorite."));
+        video2.AddComment(new Comment("Miguel", "You should add subtitles!"));
+
+        Video video3 = new Video("Tour of the Markets in Oaxaca", "TravelWithJess", 720);
+        video3.AddComment(new Comment("Emma", "Love the culture and colors!"));
+        video3.AddComment(new Comment("Daniel", "Very informative, thank you!"));
+        video3.AddComment(new Comment("Nina", "You should do a follow-up!"));
+
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        foreach (var video in videos)
+        {
+            Console.WriteLine($"Title: {video.Title}");
+            Console.WriteLine($"Author: {video.Author}");
+            Console.WriteLine($"Length: {video.LengthInSeconds} seconds");
+            Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
+
+            foreach (var comment in video.GetComments())
+            {
+                Console.WriteLine($" - {comment.Name}: {comment.Text}");
+            }
+
+            Console.WriteLine(new string('-', 40));
+        }
     }
 }
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int LengthInSeconds { get; set; }
+    private List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int lengthInSeconds)
+    {
+        Title = title;
+        Author = author;
+        LengthInSeconds = lengthInSeconds;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return _comments.Count;
+    }
+
+    public List<Comment> GetComments()
+    {
+        return _comments;
+    }
+}
