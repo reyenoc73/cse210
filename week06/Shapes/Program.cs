4234ba387a868a115f220322ad14ff7d10fdc4b2
reyenoc <ereyesramirez@byupathway.edu>
2025-06-14 13:03:10 -0600

    Shapes finished

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.Getcolor()}, Area: {shape.Getarea()}");
+        }
+
+        Square square = new Square("Red", 5);
+        Console.WriteLine($"Color: {square.Getcolor()}");
+        Console.WriteLine($"Area:{square.Getarea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Console.WriteLine($"Color: {rectangle.Getcolor()}");
+        Console.WriteLine($"Area: {rectangle.Getarea()}");
+
+        Circle circle = new Circle("Yellow", 3);
+        Console.WriteLine($"Color: {circle.Getcolor()}");
+        Console.WriteLine($"Area:{circle.Getarea()}");
     }
 }
